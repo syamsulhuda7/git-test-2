7558de5f2ce03fae98aec6ad975995f9b171a2b6
Syamsul Huda Harisul Muslimin <syamsulhuda748@gmail.com>
2024-01-15 20:46:29 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, iure placeat alias sit nostrum vitae repellendus inventore exercitationem quae ex eos animi excepturi eveniet quam maxime saepe eum, nisi cumque?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis obcaecati quae, accusantium fugit voluptates quasi nemo itaque exercitationem ex, perferendis velit ab assumenda temporibus odio dicta hic possimus nesciunt minus!</p>
 </body>
 </html>
